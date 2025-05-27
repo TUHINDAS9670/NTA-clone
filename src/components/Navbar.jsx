27057e00dbbb3540102a52632426ef3b4eff8f6f
TUHINDAS9670 <drup9672@gmail.com>
2025-05-27 18:48:14 +0530

    nav element size issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -95,7 +95,7 @@ const Navbar = () => {
               to="/Rti"
               className="relative px-3 py-2 hover:bg-blue-900 hover:text-white border-t-4 border-transparent hover:border-orange-400 transition-all"
             >
-              RTIkk
+              RTI
             </Link>
             <Link
               to="/Tender"
